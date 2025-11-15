a8092a3bc8c5dfa6cd3dfa8c3004312e3bd9ce83
Axeist <160474257+Axeist@users.noreply.github.com>
2025-11-16 01:18:40 +0530

    Mascot component updated to hide Spline watermark

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import POSDetails from "./pages/POSDetails";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import NotFound from "./pages/NotFound";
-import Mascot from "./components/Mascot";
 
 const queryClient = new QueryClient();
 
@@ -26,8 +25,6 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-        {/* Mascot overlay - appears on all pages */}
-        <Mascot />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
